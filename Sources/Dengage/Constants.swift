8d0be8cd00ae3c586d0997c0d263353e5ed0e26e
Priya Agarwal <priya@hiehq.com>
2023-05-06 03:40:11 +0530

    In app messages not to display on other screens, added scenario for screen name

--- Sources/Dengage/Constants.swift
+++ Sources/Dengage/Constants.swift
@@ -7,7 +7,7 @@ var EVENT_SERVICE_URL = "https://event.dengage.com"
 let DEVICE_EVENT_QUEUE = "device-event-queue"
 let SUBSCRIPTION_QUEUE = "subscription-queue"
 
-let SDK_VERSION = "5.26.2"
+let SDK_VERSION = "5.27.2"
 let SUIT_NAME = "group.dengage"
 let DEFAULT_CARRIER_ID = "1"
 let MESSAGE_SOURCE = "DENGAGE"
