c5a2fe81bf89aef2ae4edbed94c2776133f5ff86
egemen1986 <egemen.gulkilik@dengage.com>
2024-11-14 02:48:23 +0300

    CarouselMessage id property add

--- Sources/Dengage/DengageUI/CarouselNotificationUI/CarouselMessage.swift
+++ Sources/Dengage/DengageUI/CarouselNotificationUI/CarouselMessage.swift
@@ -1,11 +1,13 @@
 import Foundation
 struct CarouselMessage: Decodable{
+    let id: Int?
     let image: String?
     let title: String?
     let description: String?
     let targetUrl: String?
     
     init?(with dictionary:NSDictionary){
+        id = dictionary["id"] as? Int
         image = dictionary["mediaUrl"] as? String
         title = dictionary["title"] as? String
         description = dictionary["desc"] as? String
