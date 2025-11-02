514c02fd9fec73e00a35993b3a489d8e8c263c63
Priya Agarwal <priya.agarwal@dengage.com>
2025-11-02 20:56:34 +0530

    Added fix for NAN

--- Sources/Dengage/Models/Request/Event/EventRequest.swift
+++ Sources/Dengage/Models/Request/Event/EventRequest.swift
@@ -4,14 +4,17 @@ struct EventRequest: APIRequest {
     typealias Response = EmptyResponse
 
     let method: HTTPMethod = .post
-    let enpointType: EndpointType = .event
+    let endpointType: EndpointType = .event
     let path: String = "/api/event"
 
     var httpBody: Data?{
+        
+        let safeEventDetails = eventDetails.removingInvalidNumbers()
+
         let parameters = ["integrationKey": integrationKey,
                           "key": key,
                           "eventTable": eventTable,
-                          "eventDetails": eventDetails] as [String : Any]
+                          "eventDetails": safeEventDetails] as [String : Any]
         return parameters.json
     }
 
@@ -22,4 +25,41 @@ struct EventRequest: APIRequest {
     let eventTable: String
     let eventDetails: [String: Any]
 }
+
+extension Dictionary where Key == String, Value == Any {
+    func removingInvalidNumbers() -> [String: Any] {
+        var cleaned: [String: Any] = [:]
+        for (key, value) in self {
+            switch value {
+            case let num as Double where num.isNaN || !num.isFinite:
+                cleaned[key] = 0 // or NSNull(), or just skip
+            case let dict as [String: Any]:
+                cleaned[key] = dict.removingInvalidNumbers()
+            case let array as [Any]:
+                cleaned[key] = array.removingInvalidNumbers()
+            default:
+                cleaned[key] = value
+            }
+        }
+        return cleaned
+    }
+}
+
+extension Array where Element == Any {
+    func removingInvalidNumbers() -> [Any] {
+        return map { element in
+            switch element {
+            case let num as Double where num.isNaN || !num.isFinite:
+                return 0
+            case let dict as [String: Any]:
+                return dict.removingInvalidNumbers()
+            case let array as [Any]:
+                return array.removingInvalidNumbers()
+            default:
+                return element
+            }
+        }
+    }
+}
+
 
